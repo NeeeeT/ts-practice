13a3a25a4d0aabb6d79c2d8b39608a0840b20274
Nathan Cheng <37738465+NeeeeT@users.noreply.github.com>
2023-01-29 16:05:59 +0800

    Update _8*.ts

--- src/day02/_8*.ts
+++ src/day02/_8*.ts
@@ -1,13 +1,15 @@
 // 情境式定型
 function times(
   f: (index: number) => void,
-  n: number
+  n: number = 0
 ) {
   for(let i = 0; i < n; i++) {
     f(i);
   }
 }
-times(n => console.log(n), 4);
+times((n) => {
+  console.log(n)
+}, 5);
 
 // ! wrong case
 // function _f(n) {
